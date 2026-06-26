f1b5ce105ed10133a614eb46fd2c9272ba1eff5a
GeoffreyTerral <77966414+GeoffreyTerral@users.noreply.github.com>
2026-06-26 10:54:42 -0400

    Add files via upload

---
+++ OptogeneticStimulation/OptogeneticPipeline_Ratliff_Terral_2026.m
@@ -0,0 +1,5 @@
+%% Optogenetic activation of Sst-Chodl cells pipeline - Ratliff, Terral et al., 2026 - Nature
+
+
+%% pipeline for multi LFPs analyzis - figure 4
+MultiLFP_analyzis_Ratliff_Terral2026
